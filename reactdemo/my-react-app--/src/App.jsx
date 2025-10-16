ee3c264bbcbb79b0b3df64add1016480f75a22b0
Aditya arya <adityasarya2006@gmail.com>
2025-10-16 09:55:07 +0530

    Add files via upload

---
+++ reactdemo/my-react-app--/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+
+
+
+
+const App = () => {
+  return (
+    <>
+    <Header />{" "}
+    <div>
+      <h1>hello</h1>
+      <p>I am Srihari,From BNG</p>
+    </div>
+    </>
+  );
+}
+export default App;
